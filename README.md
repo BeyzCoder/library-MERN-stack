6121f80fdcc42744c47d8c15523f3ce90cae8e60
BeyzCoder <stevenbaes11@yahoo.com>
2023-09-19 20:41:16 -0600

    Create a frontend React template

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ install environment
 - node
 - express
 - mongoose
+- cors
+- npm create vite@latest -> React -> JavaScript
 
 developer tool
 - postman
@@ -11,3 +13,4 @@ learning experience
 - CRUD (Create, Read, Update, Delete)
 - Database
 - MongoDB
+
--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,19 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 app.get('/', (req, res) => {
     return res.status(234).send(`Welcome to MERN Stack`);
